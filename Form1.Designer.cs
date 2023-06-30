3cd340c09bf1f5aee52a5ea5b85e575ced79ba89
Gayoon1234 <s3843584@student.rmit.edu.au>
2023-06-30 16:05:12 +1000

    added base app functionality

---
+++ Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace ImageCopier
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
+            this.pbTitle = new System.Windows.Forms.PictureBox();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnPrev = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pbImage = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbImageNum = new System.Windows.Forms.TextBox();
+            this.lblImageCount = new System.Windows.Forms.Label();
+            this.lblImageName = new System.Windows.Forms.Label();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.fbd = new System.Windows.Forms.FolderBrowserDialog();
+            this.tbFrom = new System.Windows.Forms.TextBox();
+            this.btnFrom = new System.Windows.Forms.Button();
+            this.btnTo = new System.Windows.Forms.Button();
+            this.tbTo = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTitle)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbTitle
+            // 
+            this.pbTitle.BackColor = System.Drawing.Color.Transparent;
+            this.pbTitle.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbTitle.BackgroundImage")));
+            this.pbTitle.Location = new System.Drawing.Point(234, 16);
+            this.pbTitle.Name = "pbTitle";
+            this.pbTitle.Size = new System.Drawing.Size(138, 38);
+            this.pbTitle.TabIndex = 0;
+            this.pbTitle.TabStop = false;
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.BackColor = System.Drawing.Color.Yellow;
+            this.btnCopy.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCopy.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCopy.Location = new System.Drawing.Point(212, 487);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(194, 76);
+            this.btnCopy.TabIndex = 1;
+            this.btnCopy.Text = "Copy Image\r\n(SPACE)";
+            this.btnCopy.UseVisualStyleBackColor = false;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.pictureBox1.Location = new System.Drawing.Point(-2, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(637, 18);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnNext
+            // 
+            this.btnNext.BackColor = System.Drawing.Color.YellowGreen;
+            this.btnNext.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNext.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNext.Location = new System.Drawing.Point(441, 487);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(99, 76);
+            this.btnNext.TabIndex = 3;
+            this.btnNext.Text = "Next\r\n(Right Arrow)";
+            this.btnNext.UseVisualStyleBackColor = false;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnPrev
+            // 
+            this.btnPrev.BackColor = System.Drawing.Color.LightCoral;
+            this.btnPrev.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPrev.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPrev.Location = new System.Drawing.Point(80, 487);
+            this.btnPrev.Name = "btnPrev";
+            this.btnPrev.Size = new System.Drawing.Size(99, 76);
+            this.btnPrev.TabIndex = 4;
+            this.btnPrev.Text = "Previous\r\n(Left Arrow)";
+            this.btnPrev.UseVisualStyleBackColor = false;
+            this.btnPrev.Click += new System.EventHandler(this.btnPrev_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.LightCoral;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(599, -3);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(22, 21);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "X";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(14, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "From";
+            // 
+            // pbImage
+            // 
+            this.pbImage.BackColor = System.Drawing.Color.Transparent;
+            this.pbImage.Location = new System.Drawing.Point(39, 157);
+            this.pbImage.Name = "pbImage";
+            this.pbImage.Size = new System.Drawing.Size(543, 324);
+            this.pbImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbImage.TabIndex = 7;
+            this.pbImage.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(26, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "To";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(36, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Image";
+            // 
+            // tbImageNum
+            // 
+            this.tbImageNum.Location = new System.Drawing.Point(76, 136);
+            this.tbImageNum.Name = "tbImageNum";
+            this.tbImageNum.Size = new System.Drawing.Size(44, 20);
+            this.tbImageNum.TabIndex = 10;
+            this.tbImageNum.Leave += new System.EventHandler(this.tbImageNum_Leave);
+            // 
+            // lblImageCount
+            // 
+            this.lblImageCount.AutoSize = true;
+            this.lblImageCount.BackColor = System.Drawing.Color.Transparent;
+            this.lblImageCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblImageCount.ForeColor = System.Drawing.Color.White;
+            this.lblImageCount.Location = new System.Drawing.Point(123, 138);
+            this.lblImageCount.Name = "lblImageCount";
+            this.lblImageCount.Size = new System.Drawing.Size(34, 13);
+            this.lblImageCount.TabIndex = 11;
+            this.lblImageCount.Text = "/999";
+            // 
+            // lblImageName
+            // 
+            this.lblImageName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblImageName.BackColor = System.Drawing.Color.Transparent;
+            this.lblImageName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblImageName.ForeColor = System.Drawing.Color.White;
+            this.lblImageName.Location = new System.Drawing.Point(218, 140);
+            this.lblImageName.Name = "lblImageName";
+            this.lblImageName.Size = new System.Drawing.Size(366, 16);
+            this.lblImageName.TabIndex = 12;
+            this.lblImageName.Text = "ImageName.jpg";
+            this.lblImageName.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.BackColor = System.Drawing.Color.Orchid;
+            this.btnLoad.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLoad.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLoad.Location = new System.Drawing.Point(513, 68);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(69, 39);
+            this.btnLoad.TabIndex = 13;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = false;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // tbFrom
+            // 
+            this.tbFrom.Location = new System.Drawing.Point(51, 65);
+            this.tbFrom.Name = "tbFrom";
+            this.tbFrom.Size = new System.Drawing.Size(430, 20);
+            this.tbFrom.TabIndex = 16;
+            // 
+            // btnFrom
+            // 
+            this.btnFrom.BackColor = System.Drawing.Color.DarkGray;
+            this.btnFrom.FlatAppearance.BorderSize = 0;
+            this.btnFrom.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFrom.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFrom.Location = new System.Drawing.Point(466, 65);
+            this.btnFrom.Name = "btnFrom";
+            this.btnFrom.Size = new System.Drawing.Size(33, 20);
+            this.btnFrom.TabIndex = 17;
+            this.btnFrom.Text = "...";
+            this.btnFrom.UseVisualStyleBackColor = false;
+            this.btnFrom.Click += new System.EventHandler(this.btnFrom_Click);
+            // 
+            // btnTo
+            // 
+            this.btnTo.BackColor = System.Drawing.Color.DarkGray;
+            this.btnTo.FlatAppearance.BorderSize = 0;
+            this.btnTo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTo.Location = new System.Drawing.Point(466, 91);
+            this.btnTo.Name = "btnTo";
+            this.btnTo.Size = new System.Drawing.Size(33, 20);
+            this.btnTo.TabIndex = 19;
+            this.btnTo.Text = "...";
+            this.btnTo.UseVisualStyleBackColor = false;
+            this.btnTo.Click += new System.EventHandler(this.btnTo_Click);
+            // 
+            // tbTo
+            // 
+            this.tbTo.Location = new System.Drawing.Point(51, 91);
+            this.tbTo.Name = "tbTo";
+            this.tbTo.Size = new System.Drawing.Size(430, 20);
+            this.tbTo.TabIndex = 18;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(620, 593);
+            this.Controls.Add(this.btnTo);
+            this.Controls.Add(this.tbTo);
+            this.Controls.Add(this.btnFrom);
+            this.Controls.Add(this.tbFrom);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.lblImageName);
+            this.Controls.Add(this.lblImageCount);
+            this.Controls.Add(this.tbImageNum);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pbImage);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnPrev);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.pbTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.KeyPreview = true;
+            this.Name = "mainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ImageCopier";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.mainForm_KeyDown);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.mainForm_MouseDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pbTitle)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbTitle;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnPrev;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pbImage;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbImageNum;
+        private System.Windows.Forms.Label lblImageCount;
+        private System.Windows.Forms.Label lblImageName;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.FolderBrowserDialog fbd;
+        private System.Windows.Forms.TextBox tbFrom;
+        private System.Windows.Forms.Button btnFrom;
+        private System.Windows.Forms.Button btnTo;
+        private System.Windows.Forms.TextBox tbTo;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace ImageCopier
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private string pathFrom;
+        private string pathTo;
+        int index;
+        private bool isLoaded = false;
+        private string[] images;
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+        private void mainForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnFrom_Click(object sender, EventArgs e)
+        {
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                tbFrom.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void btnTo_Click(object sender, EventArgs e)
+        {
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                tbTo.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(tbFrom.Text) && Directory.Exists(tbTo.Text))
+            {
+                pathFrom = tbFrom.Text;
+                pathTo = tbTo.Text;
+                images = findImages(pathFrom);
+                lblImageCount.Text = "/" + images.Length;
+                setImageTo(0);
+                isLoaded = true;
+            }
+            else 
+            {
+                MessageBox.Show("Directory not valid");
+            }
+        }
+
+        private string[] findImages(string directoryPath) {
+            string[] imageExtensions = { ".jpg", ".jpeg", ".png", ".gif" };
+
+            string[] imageFilePaths = Directory.GetFiles(directoryPath, "*", SearchOption.AllDirectories);
+            imageFilePaths = Array.FindAll(imageFilePaths, path => IsImageFile(path, imageExtensions));
+
+            return imageFilePaths;
+        }
+
+        private static bool IsImageFile(string filePath, string[] imageExtensions)
+        {
+            string extension = Path.GetExtension(filePath);
+            return Array.Exists(imageExtensions, ext => ext.Equals(extension, StringComparison.OrdinalIgnoreCase));
+        }
+        private void setImageTo(int i) {
+            pbImage.ImageLocation = images[i];
+            lblImageName.Text = Path.GetFileName(images[i]);
+            tbImageNum.Text = (i+1).ToString();
+            index = i;
+        }
+
+        private void tbImageNum_Leave(object sender, EventArgs e)
+        {
+            if (isLoaded)
+            {
+                int newIndex;
+                bool tryParse = int.TryParse(tbImageNum.Text, out newIndex);
+                if (tryParse)
+                {
+                    setImageTo(newIndex - 1);
+                }
+            }
+        }
+
+        private void nextImage() { 
+            setImageTo((index+1)%images.Length);
+        }
+
+        private void prevImage()
+        {
+            setImageTo((index-1+images.Length)%images.Length);
+        }
+
+        private void copyImage() {
+            try
+            {
+                File.Copy(images[index], Path.Combine(pathTo, Path.GetFileName(images[index])));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred while copying the file: " + ex.Message);
+            }
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            nextImage();
+        }
+
+        private void btnPrev_Click(object sender, EventArgs e)
+        {
+            prevImage();
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            copyImage();
+        }
+
+        private void mainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                prevImage();
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                nextImage();
+            }
+            else if (e.KeyCode == Keys.Space) 
+            {
+                copyImage();
+            }
+        }
+
+       
+    }
+}
